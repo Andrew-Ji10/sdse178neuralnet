0a65f8f9296d1108b0b7ed8d9df5f630aa3552bd
Andrew Ji <jiandrew10@gmail.com>
2023-04-25 15:01:05 -0700

    data split into test and train

--- neuralNet.py
+++ neuralNet.py
@@ -1,7 +1,21 @@
 import sklearn
 from sklearn.neural_network import MLPClassifier
 import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+rngSeed = 45332
+
+file = pd.read_csv(r"C:\Users\jiand\SDSE178\FinalProject\sdse178neuralnet\carData.csv")
 
 length = np.array([1, 2, 3, 4])
 
-print(length.shape)
+## Clean the data & split
+desiredcolumns = file.columns[5:19]
+practicalFile = file.get(desiredcolumns)
+
+Xtrain, Xtest, ytrain, ytest = train_test_split(practicalFile[desiredcolumns[1:]],
+                                                practicalFile[desiredcolumns[0]], 
+                                                test_size=0.2, random_state=rngSeed)
+
+print(Xtrain)
